11014227c2a37a5c663356d9077e2fdbae8d8434
Moch. Faisal Ediansyah <faisalmochammad54@gmail.com>
2022-09-17 15:19:32 +0700

    Promise emotions

--- promise.js
+++ promise.js
@@ -1,7 +1,32 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotions) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    const resultTheaterIXX = [...theaterIXX];
+    const resultTheaterVCG = [...theaterVGC];
+
+    // console.log(resultTheaterIXX, resultTheaterVCG);
+
+    // Menggabungkan data theaterIXX dan theaterVGC
+    const result = resultTheaterIXX
+      .map((emotion) => emotion.hasil)
+      .concat(resultTheaterVCG.map((emotion) => emotion.hasil));
+    // console.log(result);
+
+    // Melakukan filter array sesuai dengan mood
+    emotions = result.filter((result) => result === emotions);
+    // console.log(emotions);
+
+    // Menampilkan total mood berdasarkan emosi dan tidak emosi
+    return emotions.length;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
